d1699a6c0b03f7985dcdf492b45e7087e97c6c28
alvinuy1110 <alvinuy1110@yahoo.com>
2020-03-14 16:48:31 -0400

    Init commit

---
+++ src/test/java/com/myproject/springfox/MyTest.java
@@ -0,0 +1,13 @@
+package com.myproject.springfox;
+
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+@Slf4j
+public class MyTest {
+
+    @Test
+    public  void test1() {
+        log.info("test1");
+    }
+}
